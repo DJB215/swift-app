5c0e396ae1d7d368df98d2bdf8e58e264a161857
Darrell Bullock <darrellbullock@comcast.net>
2016-09-09 17:18:30 -0400

    My first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+var age = 15
+
+if age != 15 {
+    print("We are the same age")
+}
+
+var name = "Darrell"
+
+if name == "Nick" {
+    print("We have the same name!")
+} else {
+    print("We don't have the same name :(")
+}
+
+var favoriteShow = "Sanford and Son"
+
+if favoriteShow == "Good Times" {
+    print("We don't have the same favorite show")
+} else {
+    print("We have the same favorite show!")
+}
---
+++ Swift App/Swift App/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Darrell Bullock on 9/9/16.
+//  Copyright © 2016 Darrell Bullock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func myButtonPressed(_ sender: AnyObject) {
+        
+        theLabel.text = "Buttons are cool!"
+        
+    }
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times"
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
